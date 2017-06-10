daefa92a0d1ada3993f14b246951f8384fa8fa09
DESKTOP-9U8F004\ddrgis <kasabiankpp@gmail.com>
2017-06-10 11:24:05 +0200

    + Decorator

---
+++ Decorator/DecoratedComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Decorator
+{
+    public class DecoratedComponent : Component
+    {
+        private readonly Component component;
+        private readonly string decoratedState;
+        public DecoratedComponent(Component component, string decoratedState)
+        {
+            this.component = component;
+            this.decoratedState = decoratedState;
+        }
+        public override void Operation()
+        {
+            component.Operation();
+            Console.Write(" with decorated behavior and decorated state");
+        }
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var component = new ConcreteComponent();
+            component.Operation();
+            Console.WriteLine();
+
+            var changedComponent = new DecoratedComponent(component, "new state");
+            changedComponent.Operation();
+
+            Console.ReadKey();
+        }
+    }
+}
